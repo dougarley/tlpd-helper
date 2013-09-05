8a94442fc19ee7c9274969087e2d6372598b2642
Doug Arley <sgtrama@gmail.com>
2013-09-05 19:25:28 -0400

    added a file

---
+++ tlpd.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var options = {
+  host: 'us.battle.net',
+  path: '/api/wow/character/' + process.argv[2] + '/' + process.argv[3] + '?fields=mounts'
+};
+
+http.get(options, function(res) {
+	console.log('STATUS: ' + res.statusCode);
+	console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+	res.on('data', function (data) {
+		console.log(JSON.parse(data));
+		/*
+		function find() {
+		    for (var key in chunk) {
+		        if (check.hasOwnProperty(key)) {
+		            var current = paises[key];
+
+		            if (creatureId == 32153) {
+		               console.log("true");
+		            } else { console.log("false"); }
+		        }
+		    }
+		}
+		*/
+	});
+}).on('error', function(e) {
+  console.log('ERROR: ' + e.message);
+});
