ff7861e7afb52344c752fab306c16898f28250b9
Doug Arley <sgtrama@gmail.com>
2013-09-07 20:42:49 -0400

    Lots of stuff
    -changed script to loop through provided JSON file.
    -loops through character activity feed and looks for existing TLPD mount
    -loops through character activity feed and returns any recent Vyragosa
    kills

--- tlpd.js
+++ tlpd.js
@@ -1,36 +1,68 @@
-fs = require('fs')
-fs.readFile('data.json', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  var chardata = JSON.parse(data);
-  console.log(chardata.characters[0]);
-});
-
-/*
+var chardata;
 var http = require('http');
-var options = {
-  host: 'us.battle.net',
-  path: '/api/wow/character/' + process.argv[2] + '/' + process.argv[3] + '?fields=mounts'
-};
+var linebreak = ('\n========================================\n');
+var loljson;
 
-http.get(options, function(res) {
-	//console.log('STATUS: ' + res.statusCode);
-	//console.log('HEADERS: ' + JSON.stringify(res.headers));
-	var body = '';
+fs = require('fs')
 
-	res.on('data', function (chunk) {
-		body += chunk;
-	});
+if (process.argv[2]) { 
+    loljson = process.argv[2]; 
+} else { 
+    loljson = 'data.json';
+}
 
-	res.on('end', function() {
-        var response = JSON.parse(body);
-        var daloop = response.mounts.collected;
-        for(i=0;i<daloop.length;i++) {
-        	if(daloop[i].creatureId == 32153) { console.log("TLPD = true")}
-        }
-    });
-}).on('error', function(e) {
-  console.log('ERROR: ' + e.message);
+
+fs.readFile(loljson, 'utf8', function (err,data) {
+    if (err) {
+        return console.log(err);
+    }
+    
+    chardata = JSON.parse(data);
+
+    for(t=0;t<chardata.characters.length;t++){
+        var options = {
+          host: 'us.battle.net',
+          path: '/api/wow/character/' + chardata.characters[t].charserver + '/' + chardata.characters[t].charname + '?fields=mounts,feed'
+        };
+
+        http.get(options, function(res) {
+            //console.log('STATUS: ' + res.statusCode);
+            //console.log('HEADERS: ' + JSON.stringify(res.headers));
+            var body = '';
+
+
+            res.on('data', function (chunk) {
+                body += chunk;
+            });
+
+            res.on('end', function() {
+                var response = JSON.parse(body);
+                var mntloop = response.mounts.collected;
+                var feedloop = response.feed;
+
+                console.log(linebreak);
+                console.log(response.name);
+
+                for(i=0;i<mntloop.length;i++) {
+                  if(mntloop[i].creatureId == 32153) { console.log("Has Time-Lost Protodrake") }
+                }
+
+                for(i=0;i<feedloop.length;i++) {
+                    if(feedloop[i].type ="LOOT" && feedloop[i].itemId == 44732) { 
+                        var mmToMonth = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+
+                        function tm(timestamp){
+                            var dt = new Date(timestamp);
+                            var mm = mmToMonth[dt.getMonth()];
+                            return dt.getDate() + "-" + mm + "-" + dt.getFullYear() + " " + dt.getHours() + ":" + dt.getMinutes();
+                        }
+
+                        console.log("Has killed Vyragosa:", tm(feedloop[i].timestamp));
+                    }
+                }
+            });
+        }).on('error', function(e) {
+            console.log('ERROR: ' + e.message);
+        });
+    }
 });
-*/
