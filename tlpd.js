91d32ef71a77180868ea29910e5d7770eaa65fe6
Mark Oleson <fusion2004@gmail.com>
2013-09-15 12:53:40 -0400

    hashbang for script execution, cleaned up comments a little, changed date parsing/formatting to moment.js

--- tlpd.js
+++ tlpd.js
@@ -1,18 +1,21 @@
+#! /usr/bin/env node
+
 var chardata;
 var http = require('http');
+var moment = require('moment');
 var linebreak = ('\n========================================\n');
 var loljson;
 
 fs = require('fs')
 
-//If an argument is passed, use that argument as target file, otherwise default to data.json
+// If an argument is passed, use that argument as target file, otherwise default to data.json
 if (process.argv[2]) { 
     loljson = process.argv[2]; 
 } else { 
     loljson = 'data.json';
 }
 
-//Loop through the JSON file
+// Loop through the JSON file
 fs.readFile(loljson, 'utf8', function (err,data) {
     if (err) {
         return console.log(err);
@@ -40,26 +43,18 @@ fs.readFile(loljson, 'utf8', function (err,data) {
 
                 console.log(linebreak);
 
-                //Log out character name
+                // Log out character name
                 console.log(response.name);     
 
-                //Check to see if character has TLPD
+                // Check to see if character has TLPD
                 for(i=0;i<mntloop.length;i++) {
                   if(mntloop[i].creatureId == 32153) { console.log("Has Time-Lost Protodrake") }
                 }
 
-                //Check character activity feed for recent Vyragosa loot
+                // Check character activity feed for recent Vyragosa loot
                 for(i=0;i<feedloop.length;i++) {
                     if(feedloop[i].type ="LOOT" && feedloop[i].itemId == 44732) { 
-                        var mmToMonth = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
-
-                        function tm(timestamp){
-                            var dt = new Date(timestamp);
-                            var mm = mmToMonth[dt.getMonth()];
-                            return ('0' + dt.getDate()).slice(-2) + "-" + mm + "-" + dt.getFullYear() + " " + ('0' + dt.getHours()).slice(-2) + ":" + ('0' + dt.getMinutes()).slice(-2);
-                        }
-
-                        console.log("Has killed Vyragosa:", tm(feedloop[i].timestamp));
+                        console.log("Has killed Vyragosa:", moment(feedloop[i].timestamp).format('lll'));
                     }
                 }
             });
